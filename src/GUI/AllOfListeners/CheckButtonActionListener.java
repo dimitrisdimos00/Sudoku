0e61578351838f4aa77810d28bf58c1682af33e9
Tsifoutis <tsifoutis@csd.auth.gr>
2019-12-25 13:58:06 +0200

    Its all coming together (Prosthesa getters kai setters sthn Logic gia to GUI)

--- src/GUI/AllOfListeners/CheckButtonActionListener.java
+++ src/GUI/AllOfListeners/CheckButtonActionListener.java
@@ -8,55 +8,15 @@ import java.awt.event.ActionListener;
 
 public class CheckButtonActionListener implements ActionListener {
 
-    private SudokuFrame anSudokuFrame;
+    private SudokuFrame aSudokuFrame;
 
-    public CheckButtonActionListener(SudokuFrame anSudokuFrame) {
-        this.anSudokuFrame = anSudokuFrame;
+    public CheckButtonActionListener(SudokuFrame aSudokuFrame) {
+        this.aSudokuFrame = aSudokuFrame;
     }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-        //CheckIfItBelongsToTheArray();
+
     }
 
-
-    //    private void CheckIfItBelongsToTheArray() {
-//
-//        boolean found = false;
-//
-//        if (anSudokuFrame.getNumbers()!=null) {
-//            for (int i = 0; i< anSudokuFrame.getNumOfRows(); i++) {
-//                for (int j = 0; j< anSudokuFrame.getNumOfColumns(); j++) {
-//                    for (int k = 0; k< anSudokuFrame.getNumbers().length; k++) {
-//                        if (anSudokuFrame.getTheField()[i][j].getText().equals(String.valueOf(anSudokuFrame.getNumbers()[k]))) {
-//                            found = true;
-//                            anSudokuFrame.getTheField()[i][j].setBackground(Color.green);
-//                            break;
-//                        }
-//                    }
-//                    if (!found && !(anSudokuFrame.getTheField()[i][j].getText().equals(""))) {
-//                        anSudokuFrame.getTheField()[i][j].setBackground(Color.red);
-//                    }
-//                    found = false;
-//                }
-//            }
-//        } else {
-//            for (int i = 0; i< anSudokuFrame.getNumOfRows(); i++) {
-//                for (int j = 0; j< anSudokuFrame.getNumOfColumns(); j++) {
-//                    for (int k = 0; k< anSudokuFrame.getLetters().length; k++) {
-//                        if (anSudokuFrame.getTheField()[i][j].getText().equals(Character.toString(anSudokuFrame.getLetters()[k]))) {
-//                            found = true;
-//                            anSudokuFrame.getTheField()[i][j].setBackground(Color.green);
-//                            break;
-//                        }
-//                    }
-//                    if (!found && !(anSudokuFrame.getTheField()[i][j].getText().equals(""))) {
-//                        anSudokuFrame.getTheField()[i][j].setBackground(Color.red);
-//                    }
-//                    found = false;
-//                }
-//            }
-//        }
-//
-//    }
 }
