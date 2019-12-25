04b3e6059cd576517bd29c99fa85dccaacc4ee29
Tsifoutis <tsifoutis@csd.auth.gr>
2019-12-25 19:25:08 +0200

    Its all coming together (Prosthesa getters kai setters sthn Logic gia to GUI)

--- src/GUI/AllOfListeners/CheckButtonActionListener.java
+++ src/GUI/AllOfListeners/CheckButtonActionListener.java
@@ -17,6 +17,48 @@ public class CheckButtonActionListener implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 
-    }
+        boolean found = false;
 
+        for (int row=0; row < aSudokuFrame.getNumOfRows(); row++) {
+            for (int col=0; col < aSudokuFrame.getNumOfColumns(); col++) {
+
+                if (aSudokuFrame.getTheField()[row][col].isEditable()) {
+
+                    if (aSudokuFrame.getTheField()[row][col].getText().equals("")) {
+                        aSudokuFrame.getTheField()[row][col].setBackground(Color.red);
+                        continue;
+                    }
+                    else if (aSudokuFrame.getLetters() != null) {
+                        for (int k = 0; k < aSudokuFrame.getLetters().length; k++) {
+                            if (aSudokuFrame.getTheField()[row][col].getText().equals(Character.toString(aSudokuFrame.getLetters()[k]))) {
+                                System.out.println(aSudokuFrame.getTheField()[row][col].getText());
+                                found = true;
+                                aSudokuFrame.getTheField()[row][col].setBackground(Color.white);
+                                break;
+                            }
+                        }
+                    } else {
+                        for (int k = 0; k < aSudokuFrame.getNumbers().length; k++) {
+                            if (aSudokuFrame.getTheField()[row][col].getText().equals(String.valueOf(aSudokuFrame.getNumbers()[k]))) {
+                                System.out.println(aSudokuFrame.getTheField()[row][col].getText());
+                                found = true;
+                                aSudokuFrame.getTheField()[row][col].setBackground(Color.white);
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!found) {
+                        aSudokuFrame.getTheField()[row][col].setBackground(Color.red);
+                        continue;
+                    }
+
+                    if (!(aSudokuFrame.getaLogic().insertElement(Integer.parseInt(aSudokuFrame.getTheField()[row][col].getText()), row, col))) {
+                        aSudokuFrame.getTheField()[row][col].setBackground(Color.red);
+                    }
+
+                }
+            }
+        }
+    }
 }
