e76cf72becce0f06350100d4c68255b0b667deba
Tsifoutis <tsifoutis@csd.auth.gr>
2020-01-10 11:52:48 +0200

    10/1/20 Last Changes

--- src/GUI/AllOfFrames/WinningFrame.java
+++ src/GUI/AllOfFrames/WinningFrame.java
@@ -8,25 +8,18 @@ import java.awt.*;
 
 public class WinningFrame extends JFrame {
 
-    private SudokuFrame aSudokuFrame;
-
     private String Title;
-    private String message;
     private String ButtonName;
     private String PanelName;
 
-    private JLabel winningLabel;
     private JButton winningButton;
     private Entry anEntry;
 
     public WinningFrame(SudokuFrame aSudokuFrame) {
 
-        this.aSudokuFrame = aSudokuFrame;
-
         Title = aSudokuFrame.getaSecondMenu().isFromGreekMenu() ? "Συγχαρητήρια" : "Congratulations" ;
-        message = aSudokuFrame.getaSecondMenu().isFromGreekMenu() ? "Συγχαρητήρια μόλις κέρδισες!" : "Congratulations You Won!" ;
         ButtonName = aSudokuFrame.getaSecondMenu().isFromGreekMenu() ? "Πάτα με για να κλέισει το παιχνίδι!" : "Click me to close the application" ;
-        PanelName = aSudokuFrame.getaSecondMenu().isFromGreekMenu() ? "Ψευδώνυμο" : "Νίκες / Ήττες" ;
+        PanelName = aSudokuFrame.getaSecondMenu().isFromGreekMenu() ? "Ψευδώνυμο: Νίκες / Ήττες" : "Nickname: Wins / Loses" ;
 
         this.anEntry = aSudokuFrame.getaSecondMenu().getTheEntry();
 
@@ -38,29 +31,23 @@ public class WinningFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(true);
 
-        winningLabel = new JLabel(message);
-
         WinningButtonActionListener theActionListener = new WinningButtonActionListener(this);
         winningButton = new JButton(ButtonName);
         winningButton.addActionListener(theActionListener);
 
-        FlowLayout layout = new FlowLayout();
-
-        JPanel firstPanel = new JPanel(new GridLayout(1,2));
+        JPanel firstPanel = new JPanel(new GridLayout(0,2,30,0));
         firstPanel.setBorder(BorderFactory.createTitledBorder(PanelName));
 
-        JLabel UserNameLabel = new JLabel(anEntry.getName());
+        JLabel UserNameLabel = new JLabel(anEntry.getName() + ":");
         JLabel StatsLabel = new JLabel(anEntry.getWins() + "/" + anEntry.getLosses());
 
         firstPanel.add(UserNameLabel);
         firstPanel.add(StatsLabel);
 
-        setLayout(layout);
-        add(winningLabel, BorderLayout.PAGE_START);
         add(firstPanel, BorderLayout.CENTER);
         add(winningButton, BorderLayout.PAGE_END);
 
-        setSize(280,140);
+        pack();
         setLocationRelativeTo(null);
         setVisible(true);
     }
